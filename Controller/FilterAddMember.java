cbc1424a21e637a1f38d7b9e6349d2eb10c251af
Yenni Tresnawati <yenniniszhia@gmail.com>
2016-04-13 09:23:46 +0700

    Update FilterAddMember.java

--- Controller/FilterAddMember.java
+++ Controller/FilterAddMember.java
@@ -34,57 +34,56 @@ public class FilterAddMember extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
                response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
         String nama = request.getParameter("nama");
         String alamat = request.getParameter("alamat");
         String username = request.getParameter("username");
-        String pass = request.getParameter("pass");
-        String id = request.getParameter("id");
         String nohp = request.getParameter("nohp");
+        String pass = request.getParameter("pw");
         String ttl = request.getParameter("ttl");
         String saldo = request.getParameter("saldo");
-
-Member m= new Member();
+Member m = new Member();
+m.setNama_member(nama);
+m.setTgllahir_member(ttl);
         if (m.getNama_member() == null) {
-            RequestDispatcher rd = request.getRequestDispatcher("");
+            RequestDispatcher rd = request.getRequestDispatcher("TambahMember.jsp");
             rd.forward(request, response);
-            if (m.getAlamat_member() == null) {
-                RequestDispatcher r = request.getRequestDispatcher("");
-                r.forward(request, response);
-                if (m.getUsernameM()== null) {
-                    RequestDispatcher d = request.getRequestDispatcher("");
-                    d.forward(request, response);
-                    if ((pass == null) || (id == null) || (nohp == null) || (ttl == null) || (saldo == null)) {
-                        RequestDispatcher drr = request.getRequestDispatcher("");
-                        drr.forward(request, response);
-                    }
-                }
-            }
-        } else {
+        }else if(m.getTgllahir_member()== null){
+            RequestDispatcher rd = request.getRequestDispatcher("TambahMember.jsp");
+            rd.forward(request, response);
+        }
+        
+        else {
 //         INSERT DATA MEMBER
-        Cookie ck1 = new Cookie("data1", username);
-        Member mem = new Member();
+          Cookie ck1 = new Cookie("data1", username);
+
+            Member mem = new Member();
       mem.setNama_member(nama);
       mem.setAlamat_member(alamat);
       mem.setUsernameM(username);
-      mem.setPasswordM(pass);
-      mem.setId_member(Integer.parseInt(id));
-      mem.setNohp_member(Integer.parseInt(nohp));
+      mem.setNohp_member(nohp);
+            mem.setPasswordM(pass);
+
       mem.setTgllahir_member(ttl);
       mem.setSaldo_member(Integer.parseInt(saldo));
-        Connection con = connection.Koneksi.open();
+
+        
+      Connection con = connection.Koneksi.open();
             Statement stat = con.createStatement();
-            String query2 =  "insert into MEMBER(NAMA_MEMBER,ALAMAT,USERNAME,PASSWORD,ID_MEMBER,NO_HP,TTL,SALDO) values"
-                    + "('" + mem.getNama_member() + "','" + mem.getAlamat_member() + "','" + mem.getUsernameM() + "','" + mem.getPasswordM() + "','" 
-                    + mem.getId_member()+ "','" + mem.getNohp_member() + "','" + mem.getTgllahir_member() + "','" + mem.getSaldo_member() + "')";
+            String query2 =  "insert into MEMBER(NAMA_MEMBER,ALAMAT,USERNAME,NO_HP,PASSWORD,TTL,SALDO) values"
+                    + "('" + mem.getNama_member() + "','" + mem.getAlamat_member() + "','" + mem.getUsernameM() + 
+                    "','" + mem.getNohp_member() + "','" + mem.getPasswordM() + "','" + mem.getTgllahir_member() + 
+                    "','" + mem.getSaldo_member() + "')";
             stat.executeUpdate(query2);
+                       out.println("<html><head></head><body><h2>Data berhasil ditambahkan....</h2><a href='CS.jsp'>Kembali</a></body></html>");
+            
             con.close();
 
-            RequestDispatcher rd = request.getRequestDispatcher("");
-            rd.forward(request, response);
+//            RequestDispatcher rd = request.getRequestDispatcher("CS.jsp");
+  //          rd.forward(request, response);
         }
 
 
@@ -104,7 +103,11 @@ Member m= new Member();
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            processRequest(request, response);
+                    try {
+                        processRequest(request, response);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
+                    }
         } catch (SQLException ex) {
             Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -123,7 +126,11 @@ Member m= new Member();
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            processRequest(request, response);
+                    try {
+                        processRequest(request, response);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
+                    }
         } catch (SQLException ex) {
             Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
         }
