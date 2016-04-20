4bc12c8655c867d979c3337102d1b8340e02e563
Yenni Tresnawati <yenniniszhia@gmail.com>
2016-04-20 09:37:49 +0700

    Added files via upload

---
+++ Model/CS.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author YENNI
+ */
+public class CS {
+private String username;
+private String password;
+private String namaCS;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the namaCS
+     */
+    public String getNamaCS() {
+        return namaCS;
+    }
+
+    /**
+     * @param namaCS the namaCS to set
+     */
+    public void setNamaCS(String namaCS) {
+        this.namaCS = namaCS;
+    }
+
+}
