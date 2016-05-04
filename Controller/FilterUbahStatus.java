9133f1ae7eda3b872d0a597664f199d65ddbf78d
lukasprasetya <lukasprasetya08@yahoo.com>
2016-05-04 09:39:38 +0700

    Added files via upload

---
+++ Controller/FilterUbahStatus.java
@@ -0,0 +1,125 @@
+    /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author YENNI
+ */
+public class FilterUbahStatus extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        ResultSet re;
+
+        String username = request.getParameter("username");
+        String status = request.getParameter("status");
+        datapesanan d = new datapesanan();
+
+        
+        
+        if (username == null || status == null) {
+            RequestDispatcher rd = request.getRequestDispatcher("UbahStatus.jsp");
+            rd.forward(request, response);
+
+        } else {
+            Connection con = connection.Koneksi.open();
+            Statement stat = con.createStatement();
+           // String query1 = "select STATUS from DATAPESANAN where USERNAMEM='" + username + "'";
+           // re = stat.executeQuery(query1);
+           
+          //  System.out.println(status);
+//            System.out.println(saldo2+" ditambah "+query2);
+            d.setStatus(status);
+            String query3 = "UPDATE DATAPESANAN SET STATUS='" + d.getStatus()+ "' where usernamem='" + username + "'";
+            stat.executeUpdate(query3);
+            out.println("<html><head></head><body><h2><center>STATUS BERHASIL DIUBAH</center></h2><a href='CS.jsp'>Kembali</a></body></html>");
+
+            con.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            try {
+                processRequest(request, response);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(FilterAddSaldo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FilterAddSaldo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            try {
+                processRequest(request, response);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(FilterAddSaldo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FilterAddSaldo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
