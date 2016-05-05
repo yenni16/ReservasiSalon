f3840b6a62e05c75d7809e1a443d59d1ecbbde0d
Yenni Tresnawati <yenniniszhia@gmail.com>
2016-05-05 11:22:25 +0700

    Update FilterAddMember.java

--- Controller/FilterAddMember.java
+++ Controller/FilterAddMember.java
@@ -35,7 +35,7 @@ public class FilterAddMember extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, ClassNotFoundException {
-               response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
         String nama = request.getParameter("nama");
@@ -45,45 +45,44 @@ public class FilterAddMember extends HttpServlet {
         String pass = request.getParameter("pw");
         String ttl = request.getParameter("ttl");
         String saldo = request.getParameter("saldo");
-Member m = new Member();
-m.setNama_member(nama);
-m.setTgllahir_member(ttl);
+        String status = request.getParameter("Role");
+
+        Member m = new Member();
+        m.setNama_member(nama);
+        m.setTgllahir_member(ttl);
         if (m.getNama_member() == null) {
             RequestDispatcher rd = request.getRequestDispatcher("TambahMember.jsp");
             rd.forward(request, response);
-        }else if(m.getTgllahir_member()== null){
+        } else if (m.getTgllahir_member() == null) {
             RequestDispatcher rd = request.getRequestDispatcher("TambahMember.jsp");
             rd.forward(request, response);
-        }
-        
-        else {
+        } else {
 //         INSERT DATA MEMBER
-   
 
             Member mem = new Member();
-      mem.setNama_member(nama);
-      mem.setAlamat_member(alamat);
-      mem.setUsernameM(username);
-      mem.setNohp_member(nohp);
+            mem.setNama_member(nama);
+            mem.setAlamat_member(alamat);
+            mem.setUsernameM(username);
+            mem.setNohp_member(nohp);
             mem.setPasswordM(pass);
 
-      mem.setTgllahir_member(ttl);
-      mem.setSaldo_member(Integer.parseInt(saldo));
+            mem.setTgllahir_member(ttl);
+            mem.setSaldo_member(Integer.parseInt(saldo));
+mem.setRole(status);
 
-        
-      Connection con = connection.Koneksi.open();
+            Connection con = connection.Koneksi.open();
             Statement stat = con.createStatement();
-            String query2 =  "insert into MEMBER(NAMA_MEMBER,ALAMAT,USERNAME,NO_HP,PASSWORD,TTL,SALDO) values"
-                    + "('" + mem.getNama_member() + "','" + mem.getAlamat_member() + "','" + mem.getUsernameM() + 
-                    "','" + mem.getNohp_member() + "','" + mem.getPasswordM() + "','" + mem.getTgllahir_member() + 
-                    "','" + mem.getSaldo_member() + "')";
+            String query2 = "insert into MEMBER(NAMA_MEMBER,ALAMAT,USERNAME,NO_HP,PASSWORD,TTL,SALDO,ROLE) values"
+                    + "('" + mem.getNama_member() + "','" + mem.getAlamat_member() + "','" + mem.getUsernameM()
+                    + "','" + mem.getNohp_member() + "','" + mem.getPasswordM() + "','" + mem.getTgllahir_member()
+                    + "','" + mem.getSaldo_member() +"','"+mem.getRole()+ "')";
             stat.executeUpdate(query2);
-                       out.println("<html><head></head><body><h2>Data berhasil ditambahkan....</h2><a href='CS.jsp'>Kembali</a></body></html>");
-            
+            out.println("<html><head></head><body><h2>Data berhasil ditambahkan....</h2><a href='CS.jsp'>Kembali</a></body></html>");
+
             con.close();
 
 //            RequestDispatcher rd = request.getRequestDispatcher("CS.jsp");
-  //          rd.forward(request, response);
+            //          rd.forward(request, response);
         }
 
 
@@ -103,11 +102,11 @@ m.setTgllahir_member(ttl);
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-                    try {
-                        processRequest(request, response);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+            try {
+                processRequest(request, response);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } catch (SQLException ex) {
             Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -126,11 +125,11 @@ m.setTgllahir_member(ttl);
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-                    try {
-                        processRequest(request, response);
-                    } catch (ClassNotFoundException ex) {
-                        Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+            try {
+                processRequest(request, response);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } catch (SQLException ex) {
             Logger.getLogger(FilterAddMember.class.getName()).log(Level.SEVERE, null, ex);
         }
