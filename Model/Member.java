e275270cca96fc48cbea4b25d433fec030b13eee
Yenni Tresnawati <yenniniszhia@gmail.com>
2016-05-16 17:31:35 +0700

    Update Member.java

--- Model/Member.java
+++ Model/Member.java
@@ -12,7 +12,6 @@ public class Member {
  private String tgllahir_member;
     private int saldo_member;
     private String role;
-    char namaSalah[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '*', '_', '-'};
 
     
     public String getNama_member() {
@@ -59,55 +58,11 @@ this.nama_member=nama_member;
     }
 
     public void setTgllahir_member(String tgllahir_member) {
-//        if (isTTLBenar(tgllahir_member) == true) {
-//            this.tgllahir_member = tgllahir_member;
-//        } else {
-//            this.tgllahir_member = null;
-//        }
+
 this.tgllahir_member=tgllahir_member;
     }
 
 
-    public boolean isNamaBenar(String in) {
-        char cek;
-        for (int i = 0; i < in.length(); i++) {
-            cek = in.charAt(i);
-            for (int j = 0; j < namaSalah.length; j++) {
-                if (cek == namaSalah[j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    public boolean isTTLBenar(String day) {
-        if (day.length() != 10) {
-            return false;
-        }
-        String d1 = null, d2 = null, m1 = null, m2 = null;
-        char batas[] = new char[2];
-        for (int i = 0; i < day.length(); i++) {
-            d1 = String.valueOf(day.charAt(0));
-            d2 = String.valueOf(day.charAt(1));
-            m1 = String.valueOf(day.charAt(3));
-            m2 = String.valueOf(day.charAt(4));
-            batas[0] = day.charAt(2);
-            batas[1] = day.charAt(5);
-        }
-        int hari = Integer.parseInt(d1 + d2);
-        int bulan = Integer.parseInt(m1 + m2);
-        if (hari > 31) {
-            return false;
-        } else if (bulan > 12) {
-            return false;
-        } else if ((hari > 31) && (bulan > 12)) {
-            return false;
-        } else if ((batas[0] != '/') && (batas[1] != '/')) {
-            return false;
-        }
-        return true;
-    }
 
 
     /**
